890573ff6ad0b1ae64c7ca8448bb26e963b4c8e1
VScoldness <yyao26@crimson.ua.edu>
2022-07-25 19:50:08 -0700

    finished first part of Scanner

--- vm.c
+++ vm.c
@@ -3,6 +3,7 @@
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
+#include "compiler.h"
 
 VM vm;
 
@@ -78,9 +79,15 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 #undef BINARY_OP
 }
-
+/*
 InterpretResult interpret(Chunk* chunk) {
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
     return run();
+}
+*/
+
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
