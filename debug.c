e2180daf65c4972c52a3a669a8a9a57d011b9524
VScoldness <yyao26@crimson.ua.edu>
2022-07-30 20:52:12 -0700

    finish Value part

--- debug.c
+++ debug.c
@@ -11,8 +11,20 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
+/*
 void printValue(Value value) {
-    printf("%g", value);
+    printf("%g", AS_NUMBER(value);
+}
+*/
+
+void printValue(Value value) {
+    switch (value.type) {
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NIL:       printf("nil");                  break;
+        case VAL_NUMBER:    printf("%g", AS_NUMBER(value)); break;
+    }
 }
 
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
@@ -39,6 +51,18 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_NIL:
+            return simpleInstruction("OP_NIL", offset);
+        case OP_TRUE:
+            return simpleInstruction("OP_TRUE", offset);
+        case OP_FALSE:
+            return simpleInstruction("OP_FALSE", offset);
+        case OP_EQUAL:
+            return simpleInstruction("OP_EQUAL", offset);
+        case OP_GREATER:
+            return simpleInstruction("OP_GREATER", offset);
+        case OP_LESS:
+            return simpleInstruction("OP_LESS", offset);
         case OP_ADD:
             return simpleInstruction("OP_ADD", offset);
         case OP_SUBSTRACT:
@@ -47,6 +71,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_MULTIPLY", offset);
         case OP_DIVIDE:
             return simpleInstruction("OP_DIVIDE", offset);
+        case OP_NOT:
+            return simpleInstruction("OP_NOT", offset);
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
         case OP_RETURN:
